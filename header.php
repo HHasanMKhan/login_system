5feae69abc921385722abe3b6e2a07be1900becb
HasanMullerKhan <hasanmullerkhan@gmail.com>
2021-03-05 02:41:13 +0000

    Created logout script and script to show/hide logout button

--- header.php
+++ header.php
@@ -10,18 +10,21 @@ session_start();
 
 
 	<header>
-		<form action="includes/login.inc.php" method="post">
+
+		<?php
+		if (isset($_SESSION['username'])) {
+			echo '<a href="signup.php">Register</a>
+			<form action="includes/logout.inc.php" method="post">
+			<button type="submit" name="logout-submit">Logout</button>
+			</form>';
+		}
+		else{
+			echo '<form action="includes/login.inc.php" method="post">
 			<input type="text" name="mail-uid" placeholder="Username/Email">
 			<input type="password" name="password" placeholder="Password">
 			<button type="submit" name="login-submit">Login</button>
-		</form>
-
-		<a href="signup.php">Register</a>
-
-		<form action="includes/logout.inc.php" method="post">
-			<button type="submit" name="logout-submit">Logout</button>
-		</form>
-
-
+			</form>';
+		}
+		?>
 
 	</header>
